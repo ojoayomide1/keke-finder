35fdec5137cf8be5e7cf25e3d11a0e9973414a7a
ojoayomide1 <167815462+ojoayomide1@users.noreply.github.com>
2026-04-21 17:07:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let availableKekes = [];
+
+function becomeAvailable() {
+  let locations = ["Hostel Gate", "Library", "Faculty Building", "Main Gate"];
+  
+  let randomLocation = locations[Math.floor(Math.random() * locations.length)];
+
+  let rider = {
+    name: "Keke Rider #" + (availableKekes.length + 1),
+    location: randomLocation
+  };
+
+  availableKekes.push(rider);
+
+  document.getElementById("riderMsg").innerText =
+    "You are now available at " + randomLocation;
+
+  updateList();
+}
+
+function requestKeke() {
+  if (availableKekes.length > 0) {
+    let nearest = availableKekes[0];
+
+    document.getElementById("studentMsg").innerText =
+      "🚖 " + nearest.name + " is at " + nearest.location;
+  } else {
+    document.getElementById("studentMsg").innerText =
+      "No keke available right now 😢";
+  }
+}
+
+function updateList() {
+  let list = document.getElementById("kekeList");
+  list.innerHTML = "";
+
+  availableKekes.forEach((keke) => {
+    let li = document.createElement("li");
+    li.innerText = `${keke.name} - ${keke.location}`;
+    list.appendChild(li);
+  });
+}
