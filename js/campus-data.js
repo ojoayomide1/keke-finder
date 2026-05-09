1f522053bee815dda441fa94bb30ca0cd3b9cfbe
Madara_Uchiha <ojopraise423@gmail.com>
2026-05-09 12:45:27 +0100

    Capture new campus locations: Hostel I and Behind Block B

--- js/campus-data.js
+++ js/campus-data.js
@@ -2,7 +2,24 @@
 export const CAMPUS_EDITOR_MODE = true;
 
 export const CAMPUS_MAP_DATA = {
-  locations: [],
+  "locations": [
+    {
+      "id": "hostel_i",
+      "name": "Hostel I",
+      "category": "landmark",
+      "lat": 9.286919,
+      "lng": 7.411334
+    }
+
+    ,{
+      "id": "behind_block_b",
+      "name": "Behind Block B",
+      "category": "landmark",
+      "lat": 9.289042,
+      "lng": 7.415558
+    }
+
+  ],
   paths: [],
   zones: []
 };
