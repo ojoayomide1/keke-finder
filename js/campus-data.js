bec898138bc3967e9a9f5b06f1d391a957aed8d5
Madara_Uchiha <ojopraise423@gmail.com>
2026-05-09 13:52:09 +0100

    Update campus-data.js from campus-map-editor

--- js/campus-data.js
+++ js/campus-data.js
@@ -1,6 +1,25 @@
-// Paste approved campus JSON here when you want it visible on the main app map.
+// Paste copied campus editor JSON into CAMPUS_MAP_DATA.
+export const CAMPUS_EDITOR_MODE = true;
+
 export const CAMPUS_MAP_DATA = {
-  locations: [],
+  "locations": [
+    {
+      "id": "hostel_i",
+      "name": "Hostel I",
+      "category": "landmark",
+      "lat": 9.286919,
+      "lng": 7.411334
+    }
+
+    ,{
+      "id": "behind_block_b",
+      "name": "Behind Block B",
+      "category": "landmark",
+      "lat": 9.289042,
+      "lng": 7.415558
+    }
+
+  ],
   paths: [],
   zones: []
 };
