d93c7381f176ba7d1f19017c37274a5bd7e5135a
Madara_Uchiha <ojopraise423@gmail.com>
2026-05-14 09:59:13 +0100

    fix: expose switchTab to global scope to fix bottom navigation

--- js/app.js
+++ js/app.js
@@ -191,6 +191,7 @@ function populatePathfinderLandmarks() {
 }
 
 function bindAppGlobals() {
+  window.switchTab = switchTab;
   window.toggleSidebar = toggleSidebar;
   window.switchStudentView = switchStudentView;
   window.showMap = showMap;
